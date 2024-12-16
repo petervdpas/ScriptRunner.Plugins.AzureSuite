b2f4b9de0849c66de33c11d6411e4ccf484e20c7
Peter van de Pas <peter.vdpas@protonmail.com>
2024-12-16 02:06:14 +0100

    sharedDependencies <-> 1.4.2

--- ScriptRunner.Plugins.AzureSuite/Plugin.cs
+++ ScriptRunner.Plugins.AzureSuite/Plugin.cs
@@ -22,11 +22,6 @@ namespace ScriptRunner.Plugins.AzureSuite;
     sharedDependencies: [
         "Azure.Core.dll",
         "Azure.Identity.dll",
-        "Azure.Data.Tables.dll",
-        "Azure.Messaging.ServiceBus.dll",
-        "Azure.ResourceManager.dll",
-        "Azure.Security.KeyVault.Secrets.dll",
-        "Azure.Core.Amqp.dll",
         "System.IdentityModel.Tokens.Jwt.dll",
         "System.Memory.Data.dll"
     ]
